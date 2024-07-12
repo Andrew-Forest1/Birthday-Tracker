3e96895082a0276e647b4a809e6781670870b2d4
Andrew <aforest41@yahoo.com>
2024-07-12 02:17:47 -0700

    Added Delete function to Gift

--- frontend/src/Components/Gift.js
+++ frontend/src/Components/Gift.js
@@ -1,11 +1,33 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
 import EditGift from "./EditGift"
+import { FriendsContext } from './Context';
 
 function Gift({gift, friend}){
     const [editGift, setEditGift] = useState(false)
+    const {friends, setFriends} = useContext(FriendsContext)
     
     const handleEditGift = () => {
         setEditGift(!editGift)
+    }
+
+    const handleDeleteGift = () => {
+        fetch(`/gifts/${friend.id}`, {
+            method: "DELETE"
+        })
+        .then(resp => {
+            if(resp.ok){
+                if(!friend.gifts){
+                    friend = friends[friends.findIndex((e) => {return e.id == gift.friend.id})]
+                }
+                const index = friend.gifts.findIndex((e) => {return e.id == gift.id})
+                friend.gifts = [...friend.gifts.slice(0, index), ...friend.gifts.slice(index + 1)]
+                const index2 = friends.findIndex((e) => {return e.id == friend.id})
+                setFriends([...friends.slice(0, index2), friend, ...friends.slice(index2 + 1)])
+                setEditGift(false)
+            }else{
+                resp.json().then(messageObj => alert(messageObj.error))
+            }
+        })
     }
 
     return(
@@ -17,6 +39,7 @@ function Gift({gift, friend}){
                 <a href={gift.link}>Link</a>
                 <p></p>
                 <button onClick={handleEditGift}>Edit</button>
+                <button onClick={handleDeleteGift}>Remove Gift</button>
             </>}
         </div>
     )
